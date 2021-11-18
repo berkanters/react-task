90c61bbe8c4ba0f3e9543f9f5652c1af536e1a11
berkanters <berkantersoy1995@gmail.com>
2021-11-18 21:04:22 +0300

    fonksiyonalite

--- src/screens/staredRepos/index.js
+++ src/screens/staredRepos/index.js
@@ -1,9 +1,49 @@
 import "./style.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 
 const HomeScreen = () => {
-  return (
-    <div>
-      <h1>Stared Repos</h1>
+
+  const [item, setItem] = useState();
+  const [repos, setRepos] = useState();
+
+  useEffect(() => {
+    //fetch("https://api.github.com/users/berkanters").then(response => response.json()).then(response => setItem(response));
+    //fetch("https://api.github.com/users/berkanters/repos").then(response => response.json()).then(response => setRepos(response));
+    axios
+      .get("https://api.github.com/users/berkanters").then(res=>setItem(res.data))
+    axios
+      .get("https://api.github.com/users/berkanters/repos").then(res=>setRepos(res.data))
+  }, []);
+
+
+  return (<div className="container col-md-12">
+      {item && <div>
+        <p>Name: {item.name}</p>
+        <p>Location: {item.location}</p>
+        <p>Company: {item.company}</p>
+        <p>Avatar Url: {item.avatar_url}</p>
+        <p>Public Repos: {item.public_repos}</p>
+        <p>Followers: {item.followers}</p>
+        <p>Following: {item.following}</p>
+      </div>}
+
+      {repos && repos.map(data => {
+
+
+        return <div>
+          <p>Name: {data.name}</p>
+          <p>Created at: {data.created_at}</p>
+          <Link to={data.url}> <p>Repo Url: {data.url}</p></Link>
+          <p>Repo Language: {data.language}</p>
+          <button> <p>Favorite: {data.stargazers_count}</p></button>
+
+        </div>;
+      })}
+
+
     </div>
   );
 };
